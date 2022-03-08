1cd36dfa825b3f759688cc0807c744d28ebe2c0e
younggyo <lee4519698@hanmail.net>
2022-03-08 23:01:31 +0900

    3월 1주차 멘토링 내용

---
+++ basic.js
@@ -0,0 +1,27 @@
+// 1. 출력
+console.log("첫번째 콘솔");
+
+// 2. 변수, 상수
+let name = "이영교";
+console.log(name);
+
+name = "최윤형";
+console.log(name);
+
+const age = "나이";
+console.log(age);
+
+// 3. 자료형
+const x = 1;
+const y = "문자열";
+const t = true;
+
+console.log(x, y, t);
+
+console.log(x + 1);
+
+console.log(y + 1);
+
+if (false) {
+    console.log("실행");
+}
